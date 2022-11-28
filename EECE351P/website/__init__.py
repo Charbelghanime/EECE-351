10729a5367e87726e2804e8ec59675ecfc823b8e
joseph-aoun <106529665+joseph-aoun@users.noreply.github.com>
2022-11-28 12:40:14 +0200

    Update __init__.py

--- EECE351P/website/__init__.py
+++ EECE351P/website/__init__.py
@@ -13,9 +13,9 @@ def create_app():
     db.init_app(app)
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
-    app.config['MAIL_PASSWORD'] = 'xxx'
+    app.config['MAIL_PASSWORD'] = 'qqtzxvxjrqohpopj'
     app.config['MAIL_SUPPRESS_SEND '] = False
-    app.config['MAIL_DEFAULT_SENDER'] = 'jaquelinesarrouh2016@gmail.com'
+    app.config['MAIL_DEFAULT_SENDER'] = 'eece351hotel@gmail.com'
     app.config['SECURITY_SALT'] = "aisduasdnfgnfasuiohfoqwiufh"
     mail.init_app(app)
 
