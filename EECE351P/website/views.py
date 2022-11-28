5e05e6725f8eb1b53a2554f212a2e87c1d8b9e6f
joseph-aoun <106529665+joseph-aoun@users.noreply.github.com>
2022-11-28 11:15:16 +0200

    Update views.py

--- EECE351P/website/views.py
+++ EECE351P/website/views.py
@@ -7,5 +7,7 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
+        if current_user.is_authenticated:
+            return redirect(url_for('auth.Book'))
         return redirect(url_for('auth.sign_up'))
     return render_template("home.html", user = current_user)
